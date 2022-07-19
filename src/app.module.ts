bc08b06117b1e2555a2b9e25304b5bbad8a8862c
Luqman Rasat <luqmanrasat@goodpeople.my>
2022-07-19 13:06:26 +0800

    setup db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContactInfo } from './entities/contact-info-entity';
+import { Employee } from './entities/employee.entity';
+import { Meeting } from './entities/meeting.entity';
+import { Task } from './entities/task.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'postgres',
+      username: 'postgres',
+      password: 'pass123',
+      entities: ['dist/**/*.entity.js'],
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+    TypeOrmModule.forFeature([Employee, ContactInfo, Meeting, Task]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
